657c4cdf8e950ee20f4f37271c173ccd3722028a
Justin Blau <justin.blau@oracle.com>
2020-02-07 12:04:52 -0500

    as

--- cifarcopy.py
+++ cifarcopy.py
@@ -10,14 +10,14 @@ import shutil
 # path 
 path = '/home/opc/cifar-10-batches-py/'
 
-for y in range(2,200):
+for y in range(0,200):
     j = y*5
     for x in range(1,6):
         # Source path 
         source = path + 'data_batch_'+ str(x)
         
         # Destination path 
-        destination = path + 'data_batch_'+ str(j+x)
+        destination = '/mnt/cifar-data/' + 'data_batch_'+ str(j+x)
         
         # Copy the content of 
         # source to destination 
