a9528a4fcca680b3e2c4629f09c9cc49c6c522a2
Justin Blau <justin.blau@oracle.com>
2020-02-07 10:52:53 -0500

    as

---
+++ cifarcopy.py
@@ -0,0 +1,25 @@
+
+# Python program to explain shutil.copyfile() method  
+    
+# importing os module  
+import os 
+  
+# importing shutil module  
+import shutil 
+  
+# path 
+path = '/home/opc/cifar-10-batches-py/'
+
+for y in range(2,5):
+    j = y*5
+    for x in range(1,6):
+        # Source path 
+        source = path + 'data_batch_'+ str(x)
+        
+        # Destination path 
+        destination = path + 'data_batch_'+ str(j+x)
+        
+        # Copy the content of 
+        # source to destination 
+        dest = shutil.copyfile(source, destination) 
+
