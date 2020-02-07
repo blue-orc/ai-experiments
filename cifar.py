cb34e8230d337dd1f962a72c4ab90ec05f594679
Justin Blau <justin.blau@oracle.com>
2020-02-07 10:29:15 -0500

    a

--- cifar.py
+++ cifar.py
@@ -92,7 +92,7 @@ for epoch in range(2):  # loop over the dataset multiple times
 
             # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
+            if i % 10 == 9:    # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
                     (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
